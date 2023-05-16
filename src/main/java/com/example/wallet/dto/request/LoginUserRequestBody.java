805310d594449ed20ac8d93fd30b30cc205a9dc8
FaizBShah <faizbshah2001@gmail.com>
2023-05-16 23:18:14 +0530

    Removed @ToString annotation from DTO records

--- src/main/java/com/example/wallet/dto/request/LoginUserRequestBody.java
+++ src/main/java/com/example/wallet/dto/request/LoginUserRequestBody.java
@@ -1,7 +1,4 @@
 package com.example.wallet.dto.request;
 
-import lombok.ToString;
-
-@ToString
 public record LoginUserRequestBody(String email, String password) {
 }
--- src/main/java/com/example/wallet/dto/request/RegisterUserRequestBody.java
+++ src/main/java/com/example/wallet/dto/request/RegisterUserRequestBody.java
@@ -1,7 +1,4 @@
 package com.example.wallet.dto.request;
 
-import lombok.ToString;
-
-@ToString
 public record RegisterUserRequestBody(String firstName, String lastName, String email, String password) {
 }
