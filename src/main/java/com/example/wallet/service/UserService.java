698ac75e09319d8e8eea48fd8cc8fbfb074e1075
FaizBShah <faizbshah2001@gmail.com>
2023-05-16 11:12:54 +0530

    Created logic for user repository and user service

---
+++ src/main/java/com/example/wallet/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.wallet.service;
+
+import com.example.wallet.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User does not exist with email " + email));
+    }
+}
